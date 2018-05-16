d1c65ad9b166916ee67f8750e8362b6d59dec717
sehgalmayank001 <mayank.sehgal@maropost.com>
2018-05-16 07:23:15 +0530

    minor fix

--- lib/payments/transaction.rb
+++ lib/payments/transaction.rb
@@ -18,7 +18,7 @@ module Payments
       return JSON.load(response.body)
     end
 
-    def self.authorize gateway, amount, credit_card, ip, options={}
+    def self.authorize gateway, credentials, amount, credit_card, ip, options={}
       response = HTTParty.post(
         Payments.configuration.host + '/transactions/authorize',
         body: { gateway: gateway, credentials: credentials, amount: amount, credit_card: credit_card, ip: ip, options: options },
@@ -27,7 +27,7 @@ module Payments
       return JSON.load(response.body)
     end
 
-    def self.capture gateway, amount, authorization, ip, options={}
+    def self.capture gateway, credentials, amount, authorization, ip, options={}
       response = HTTParty.post(
         Payments.configuration.host + '/transactions/capture',
         body: { gateway: gateway, credentials: credentials, amount: amount, authorization: authorization, ip: ip, options: options },
@@ -36,7 +36,7 @@ module Payments
       return JSON.load(response.body)
     end
 
-    def self.void gateway, authorization, ip, options={}
+    def self.void gateway, credentials, authorization, ip, options={}
       response = HTTParty.post(
         Payments.configuration.host + '/transactions/void',
         body: { gateway: gateway, credentials: credentials, authorization: authorization, ip: ip, options: options },
@@ -45,7 +45,7 @@ module Payments
       return JSON.load(response.body)
     end
 
-    def self.refund gateway, amount, authorization, ip, options={}
+    def self.refund gateway, credentials, amount, authorization, ip, options={}
       response = HTTParty.post(
         Payments.configuration.host + '/transactions/refund',
         body: { gateway: gateway, credentials: credentials, amount: amount, authorization: authorization, ip: ip, options: options },
@@ -54,10 +54,10 @@ module Payments
       return JSON.load(response.body)
     end
 
-    def self.verify gateway, credit_card, options={}
+    def self.verify gateway, credentials, credit_card, ip, options={}
       response = HTTParty.post(
         Payments.configuration.host + '/transactions/verify',
-        body: { gateway: gateway, credentials: credentials, credit_card: credit_card, options: options },
+        body: { gateway: gateway, credentials: credentials, credit_card: credit_card, ip: ip, options: options },
         basic_auth: Payments.configuration.auth
       )
       return JSON.load(response.body)
